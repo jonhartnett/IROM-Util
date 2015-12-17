51d95f83e34314f0bdd1462989b37d168e508d59
f4113nb34st <thefatgamer99@yahoo.com>
2015-12-16 19:13:57 -0600

    Improved internal code and made referenced assemblies params.

--- Util/RuntimeCompiler.cs
+++ Util/RuntimeCompiler.cs
@@ -16,11 +16,13 @@
 		/// <param name="code">The code file to compile.</param>
 		/// <param name="assemblies">The references assemblies of the code.</param>
 		/// <returns>The resulting assembly.</returns>
-		public static Assembly Compile(string code, string[] assemblies = null)
+		public static Assembly Compile(string code, params string[] assemblies)
 		{
 			//compile code
 			CSharpCodeProvider provider = new CSharpCodeProvider();
-			CompilerParameters paras = new CompilerParameters(AddSystemDll(assemblies));
+			CompilerParameters paras = new CompilerParameters(assemblies);
+			paras.ReferencedAssemblies.Add("System.dll");
+			paras.GenerateInMemory = true;
 			paras.CompilerOptions = "/unsafe";
 			CompilerResults results = provider.CompileAssemblyFromSource(paras, code);
 			
@@ -34,21 +36,6 @@
 			
 			//return compiled assembly
 			return results.CompiledAssembly;
-		}
-		
-		/// <summary>
-		/// Adds "System.dll" to the given assemblies.
-		/// </summary>
-		private static string[] AddSystemDll(string[] assemblies)
-		{
-			if(assemblies == null)
-			{
-				return new string[]{"System.dll"};
-			}
-			string[] result = new string[assemblies.Length + 1];
-			Array.Copy(assemblies, result, assemblies.Length);
-			result[result.Length - 1] = "System.dll";
-			return result;
 		}
 	}
 }
